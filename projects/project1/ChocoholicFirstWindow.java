c3dde9786964543021f77b3bc3cbb151187d8b0b
IDReaper <shawn.reaper01@gmail.com>
2013-11-04 22:42:49 -0500

    Added tabs for ChocAn operator and added logout buttons for all panels.

--- projects/project1/ChocoholicFirstWindow.java
+++ projects/project1/ChocoholicFirstWindow.java
@@ -20,6 +20,7 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 
@@ -28,8 +29,9 @@ public class ChocoholicFirstWindow extends JFrame implements ActionListener {
 
   private JButton toggleButton;
   private JButton submitButton;
+  private JButton logoutButton, logout2Button, logout3Button, logout4Button, logout5Button;
   private JPanel topPanel, bottom, login;
-  private JPanel mpanel, ppanel, spanel;
+  private JPanel mpanel, ppanel;
   private JPanel apanel, dpanel, upanel, vpanel;
   private JTabbedPane member;
   private JLabel topPanel2;
@@ -50,11 +52,12 @@ public class ChocoholicFirstWindow extends JFrame implements ActionListener {
     topPanel.add(new JTextArea("User ID#"), BorderLayout.SOUTH);
     topPanel2 = new JLabel();
     topPanel2.setIcon(icon);
-    topPanel2.setBorder(new EmptyBorder(new Insets(20, 0, 0, 150)));
+    topPanel2.setBorder(new EmptyBorder(new Insets(20, 0, 0, 0)));
+    topPanel2.setAlignmentX(CENTER_ALIGNMENT);
     panel.add(topPanel2);
     panel.add(topPanel);
     
-
+    
     //Radio Buttons Panel for member and provider
     bottom = new JPanel(new FlowLayout(FlowLayout.CENTER));
     ButtonGroup group = new ButtonGroup(); 
@@ -78,7 +81,7 @@ public class ChocoholicFirstWindow extends JFrame implements ActionListener {
     oRadio.setActionCommand("ChocAn Operator");
     group.add(oRadio);
     bottom.add(oRadio);
-    pRadio.addActionListener(this);
+    oRadio.addActionListener(this);
     bottom.add(Box.createRigidArea(new Dimension(0, 0)));
     panel.add(bottom);
 
@@ -91,33 +94,150 @@ public class ChocoholicFirstWindow extends JFrame implements ActionListener {
     login.add(Box.createRigidArea(new Dimension(0, 5)));
     panel.add(login);
     
+    //Logout Button
+    logoutButton = new JButton("Logout");
+    logoutButton.setActionCommand("Logout");
+    logoutButton.addActionListener(this);
+    
+    //Logout Button the sequel
+    logout2Button = new JButton("Logout");
+    logout2Button.setActionCommand("Logout2");
+    logout2Button.addActionListener(this);
+    
+    //Logout Button the trilogy
+    logout3Button = new JButton("Logout");
+    logout3Button.setActionCommand("Logout3");
+    logout3Button.addActionListener(this);
+    
+    //Logout Button the 4th?
+    logout4Button = new JButton("Logout");
+    logout4Button.setActionCommand("Logout4");
+    logout4Button.addActionListener(this);
+    
+    //Logout Button the to many sequels
+    logout5Button = new JButton("Logout");
+    logout5Button.setActionCommand("Logout5");
+    logout5Button.addActionListener(this);
+    
+    //Member update tab
     mpanel = new JPanel();
     mpanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-    mpanel.setLayout(new GridLayout(7, 2, -200, 5));
-    //mpanel.setLayout(new BoxLayout(mpanel, BoxLayout.Y_AXIS));
-    //mpanel.setBorder(new EmptyBorder(new Insets(20, 225, 15, 25)));
-    //mpanel.add(Box.createRigidArea(new Dimension(0, 10)));
+    mpanel.setLayout(new GridLayout(8, 2, -200, 5));
     mpanel.add(new JLabel("Member name:"));
     mpanel.add(new JTextArea("")); 
     mpanel.add(new JLabel("Member number:"));    
     mpanel.add(new JTextArea(""));    
-    mpanel.add(new JLabel("Member street address"));    
+    mpanel.add(new JLabel("Member street address:"));    
     mpanel.add(new JTextArea(""));    
-    mpanel.add(new JLabel("Member city"));    
+    mpanel.add(new JLabel("Member city:"));    
     mpanel.add(new JTextArea(""));    
-    mpanel.add(new JLabel("Member state"));    
+    mpanel.add(new JLabel("Member state:"));    
     mpanel.add(new JTextArea(""));    
-    mpanel.add(new JLabel("Member ZIP code"));    
+    mpanel.add(new JLabel("Member ZIP code:"));    
     mpanel.add(new JTextArea(""));    
-    mpanel.add(Box.createRigidArea(new Dimension(0, 0)));
+    mpanel.add(Box.createRigidArea(new Dimension(0,0)));
     submitButton = new JButton("Submit");
     mpanel.add(submitButton);
+    mpanel.add(Box.createRigidArea(new Dimension(0,0)));
+    mpanel.add(logoutButton);
    
-    ppanel = new JPanel(new BorderLayout(0, 0));
-    ppanel.add(new JTextArea("Service Date: 10/14/2013\nName: Myself I"), null);
+    //Provider update panel
+    ppanel = new JPanel();
+    ppanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+    ppanel.setLayout(new GridLayout(8, 2, -200, 5));
+    ppanel.add(new JLabel("Provider name:"));
+    ppanel.add(new JTextArea("")); 
+    ppanel.add(new JLabel("Provider number:"));    
+    ppanel.add(new JTextArea(""));    
+    ppanel.add(new JLabel("Provider street address:"));    
+    ppanel.add(new JTextArea(""));    
+    ppanel.add(new JLabel("Provider city:"));    
+    ppanel.add(new JTextArea(""));    
+    ppanel.add(new JLabel("Provider state:"));    
+    ppanel.add(new JTextArea(""));    
+    ppanel.add(new JLabel("Provider ZIP code:"));    
+    ppanel.add(new JTextArea(""));    
+    ppanel.add(Box.createRigidArea(new Dimension(0,0)));
+    submitButton = new JButton("Submit");
+    ppanel.add(submitButton);
+    ppanel.add(Box.createRigidArea(new Dimension(0,0)));
+    logoutButton = new JButton("Logout");
+    ppanel.add(logout2Button);
     
-    spanel = new JPanel(new BorderLayout(0, 0));
-    spanel.add(new JTextArea("LOLOLOLOLO"),null);
+    //ChocAn ADD panel
+    apanel = new JPanel();
+    apanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+    apanel.setLayout(new GridLayout(8, 2, -200, 5));
+    apanel.add(new JLabel("Member/Provider name:"));
+    apanel.add(new JTextField("")); 
+    apanel.add(new JLabel("Member/Provider number:"));    
+    apanel.add(new JTextArea(""));    
+    apanel.add(new JLabel("Member/Provider street address:"));    
+    apanel.add(new JTextArea(""));    
+    apanel.add(new JLabel("Member/Provider city:"));    
+    apanel.add(new JTextArea(""));    
+    apanel.add(new JLabel("Member/Provider state:"));    
+    apanel.add(new JTextArea(""));    
+    apanel.add(new JLabel("Member/Provider ZIP code:"));    
+    apanel.add(new JTextArea(""));    
+    apanel.add(Box.createRigidArea(new Dimension(0,0)));
+    submitButton = new JButton("ADD");
+    apanel.add(submitButton);
+    apanel.add(Box.createRigidArea(new Dimension(0,0)));
+    logoutButton = new JButton("Logout");
+    apanel.add(logout3Button);
+    
+    //ChocAn DELETE panel
+    dpanel = new JPanel();
+    dpanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+    dpanel.setLayout(new GridLayout(8, 2, -200, 5));
+    dpanel.add(new JLabel("Member/Provider name:"));
+    dpanel.add(new JTextArea("")); 
+    dpanel.add(new JLabel("Member/Provider number:"));    
+    dpanel.add(new JTextArea(""));    
+    dpanel.add(new JLabel("Member/Provider street address:"));    
+    dpanel.add(new JTextArea(""));    
+    dpanel.add(new JLabel("Member/Provider city:"));    
+    dpanel.add(new JTextArea(""));    
+    dpanel.add(new JLabel("Member/Provider state:"));    
+    dpanel.add(new JTextArea(""));    
+    dpanel.add(new JLabel("Member/Provider ZIP code:"));    
+    dpanel.add(new JTextArea(""));    
+    dpanel.add(Box.createRigidArea(new Dimension(0,0)));
+    submitButton = new JButton("DELETE");
+    dpanel.add(submitButton);
+    dpanel.add(Box.createRigidArea(new Dimension(0,0)));
+    logoutButton = new JButton("Logout");
+    dpanel.add(logout4Button);
+    
+    //ChocAn UPDATE panel
+    upanel = new JPanel();
+    upanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+    upanel.setLayout(new GridLayout(8, 2, -200, 5));
+    upanel.add(new JLabel("Member/Provider name:"));
+    upanel.add(new JTextArea("")); 
+    upanel.add(new JLabel("Member/Provider number:"));    
+    upanel.add(new JTextArea(""));    
+    upanel.add(new JLabel("Member/Provider street address:"));    
+    upanel.add(new JTextArea(""));    
+    upanel.add(new JLabel("Member/Provider city:"));    
+    upanel.add(new JTextArea(""));    
+    upanel.add(new JLabel("Member/Provider state:"));    
+    upanel.add(new JTextArea(""));    
+    upanel.add(new JLabel("Member/Provider ZIP code:"));    
+    upanel.add(new JTextArea(""));    
+    upanel.add(Box.createRigidArea(new Dimension(0,0)));
+    submitButton = new JButton("UPDATE");
+    upanel.add(submitButton);
+    upanel.add(Box.createRigidArea(new Dimension(0,0)));
+    logoutButton = new JButton("Logout");
+    upanel.add(logout5Button);
+    
+    //ppanel = new JPanel(new BorderLayout(0, 0));
+    //ppanel.add(new JTextArea("Service Date: 10/14/2013\nName: Myself I"), null);
+    
+    //spanel = new JPanel(new BorderLayout(0, 0));
+    //spanel.add(new JTextArea("LOLOLOLOLO"),null);
 
     //Member information tabbed pane 1
     member = new JTabbedPane();
@@ -141,40 +261,108 @@ public class ChocoholicFirstWindow extends JFrame implements ActionListener {
     //Print out the action retrieved
     System.out.println(event.getActionCommand());
     
-    //Changes a state switch is member or provider is selected
+    //Changes state to zero if member is selected
     if ("Member".equals(event.getActionCommand())){
       state = 0;
     }
     
+    //Changes state to one if Provider is selected
     if ("Provider".equals(event.getActionCommand())){
       state = 1;
+    }
+    
+    //Changes state to one if Provider is selected
+    if ("ChocAn Operator".equals(event.getActionCommand())){
+      state = 2;
+    }
+    
+    //Logout button
+    if ("Logout".equals(event.getActionCommand())){
+    	member.setVisible(false);
+    	member.removeAll();
+    	topPanel.setVisible(true);
+      topPanel2.setVisible(true);
+      bottom.setVisible(true);
+      login.setVisible(true);
+    }
+    
+    //Logout button the sequel
+    if ("Logout2".equals(event.getActionCommand())){
+    	member.setVisible(false);
+    	member.removeAll();
+    	topPanel.setVisible(true);
+      topPanel2.setVisible(true);
+      bottom.setVisible(true);
+      login.setVisible(true);
+    }
+    
+    //Logout button 3
+    if ("Logout3".equals(event.getActionCommand())){
+    	member.setVisible(false);
+    	member.removeAll();
+    	topPanel.setVisible(true);
+      topPanel2.setVisible(true);
+      bottom.setVisible(true);
+      login.setVisible(true);
+    }
+    
+    //Logout button 4
+    if ("Logout4".equals(event.getActionCommand())){
+    	member.setVisible(false);
+    	member.removeAll();
+    	topPanel.setVisible(true);
+      topPanel2.setVisible(true);
+      bottom.setVisible(true);
+      login.setVisible(true);
+    }
+    
+    //Logout button 5
+    if ("Logout5".equals(event.getActionCommand())){
+    	member.setVisible(false);
+    	member.removeAll();
+    	topPanel.setVisible(true);
+      topPanel2.setVisible(true);
+      bottom.setVisible(true);
+      login.setVisible(true);
     }
     
     //When login is selected, depending on the state switch, a set of tabbed panes are added
     if ("Login".equals(event.getActionCommand())){
       if (state == 0){         
+      	member.setVisible(true);
         topPanel.setVisible(false);
         topPanel2.setVisible(false);
         bottom.setVisible(false);
         login.setVisible(false);
 
         member.addTab("Member Information", mpanel); 
-        member.addTab("Provider Information", null, ppanel);
-        member.addTab("Services Provided", null, spanel);
+        //member.addTab("Provider Information", null, ppanel);
+        //member.addTab("Services Provided", null, spanel);
+        
         
       }
-      else{
+      else if (state == 1){
+      	member.setVisible(true);
         topPanel.setVisible(false);
         topPanel2.setVisible(false);
         bottom.setVisible(false);
         login.setVisible(false);
         
-        member.addTab("ADD", apanel);        
-        member.addTab("DELETE", dpanel);        
-        member.addTab("UPDATE", upanel);        
-        member.addTab("VIEW", vpanel);
-        
+        member.addTab("Provider Information", ppanel);        
       }
+      else{
+      	member.setVisible(true);
+        topPanel.setVisible(false);
+        topPanel2.setVisible(false);
+        bottom.setVisible(false);
+        login.setVisible(false);
+        
+        member.addTab("Add Member/Provider", apanel);
+        member.addTab("Delete Member/Provider", dpanel);
+        member.addTab("Update Member/Provider", upanel);
+      
+      }
+      
     } 
   }
 
